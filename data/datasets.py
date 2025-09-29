c1c67be7bd8c4475ba4b12063b8035bce87c3be7
JericCantos <cantos.jon.eric@gmail.com>
2025-09-29 16:18:14 +0200

    included links to data sets, added the relevant plots to the outputs, and added the tuned and vanilla XGB models to the repo for re-use

---
+++ data/datasets.py
@@ -0,0 +1,21 @@
+
+
+file_urls = {
+    # Original Tables
+    "holiday_events": "https://drive.google.com/uc?id=1RMjSuqHXHTwAw_PGD5XVjhA3agaAGHDH",
+    "items": "https://drive.google.com/uc?id=1ogMRixVhNY6XOJtIRtkRllyOyzw1nqya",
+    "oil": "https://drive.google.com/uc?id=1Q59vk2v4WQ-Rpc9t2nqHcsZM3QWGFje_",
+    "stores": "https://drive.google.com/uc?id=1Ei0MUXmNhmOcmrlPad8oklnFEDM95cDi",
+    "train": "https://drive.google.com/uc?id=1oEX8NEJPY7wPmSJ0n7lO1JUFYyZjFBRv",
+    "transactions": "https://drive.google.com/uc?id=1PW5LnAEAiL43fI5CRDn_h6pgDG5rtBW_",
+
+    # Pickled transactions
+    "preliminary": "https://drive.google.com/file/d/1-slDnCsV-V-v1aS7maViZPiHIbLDWOv3",
+        # contains rows of "train" belonging to the Guayas region, and only relating
+        # to products from the top 3 families (GROCERY I,  BEVERAGES, and CLEANING)
+    "cleaned": "https://drive.google.com/file/d/1otcA0OoYn1QnG-hU-zKA-QfFsJ7g4HUX"
+        # preliminary dataset with days with 0 sales filled in, outliers tagged and 
+        # winsorized, boolean flags set (including holiday marker), merged with df_items
+        # and df_stores to include some item- and store-level information, and with 
+        # date components extracted.
+}
